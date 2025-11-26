b30209d1286a64d63b61cdb64d139d30f01d10f1
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2025-11-26 10:55:38 +0530

    edit and delete category

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,12 +1,37 @@
 package com.ecom.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@Autowired
+	private CategoryService categoryService;
+	
 	
 	@GetMapping("/")
 	public String index() {
@@ -23,10 +48,101 @@ public class AdminController {
 	}
 	
 	@GetMapping("/category")
-	public String category() {
+	public String category(Model m) {
 		
-		
+		m.addAttribute("categorys",categoryService.getAllCategory());
 		return "admin/category";
+	}
+	
+	
+	@PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category category,@RequestParam("file") MultipartFile file ,HttpSession session) throws IOException {
+		
+		String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
+		category.setImageName(imageName);
+		
+		Boolean existCategory = categoryService.existCategory(category.getName());
+		
+		if(existCategory) {
+			session.setAttribute("errorMsg", "Category already exist!!!");
+		}else {
+			Category saveCategory = categoryService.saveCategory(category);
+			
+			if(ObjectUtils.isEmpty(saveCategory)) {
+				session.setAttribute("errorMsg", "Not Saved, Internal Server Error !");
+			}else {
+				
+				File saveFile= new ClassPathResource("static/img").getFile();
+			    Path path =	Paths.get(saveFile.getAbsolutePath()+File.separator+"category_img"+File.separator+file.getOriginalFilename());
+			    
+			    System.out.println(path);
+			    
+			    Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				
+				session.setAttribute("succMsg", "Saved Successfully !");
+			}
+			
+		}
+		
+		return "redirect:/admin/category";
+	}
+	
+	
+	@GetMapping("/deleteCategory/{id}")
+	public String deleteCategory(@PathVariable int id, HttpSession session) {
+		
+		Boolean deleteCategory = categoryService.deleteCategory(id);
+		
+		if(deleteCategory) {
+			session.setAttribute("succMsg", "category deleted successfully");
+		}else {
+			session.setAttribute("errorMsg", "something wrong on server");
+		}
+		
+		return "redirect:/admin/category";
+	}
+	
+	@GetMapping("/loadEditCategory/{id}")
+	public String loadEditCategory(@PathVariable int id, Model m) {
+		
+		m.addAttribute("category",categoryService.getCategoryById(id) );
+		
+		return "admin/edit_category";
+	}
+	
+	@PostMapping("/updateCategory")
+	public String updateCategory(@ModelAttribute Category category,@RequestParam("file") MultipartFile file, HttpSession session) throws IOException {
+		
+		Category oldcategory = categoryService.getCategoryById(category.getId());
+		String imageName = file.isEmpty() ? oldcategory.getImageName() : file.getOriginalFilename();
+		
+		if(!ObjectUtils.isEmpty(category))
+		{
+			oldcategory.setName(category.getName());
+			oldcategory.setIsActive(category.getIsActive());
+			oldcategory.setImageName(imageName);
+		}
+		
+		Category updateCategory = categoryService.saveCategory(oldcategory);
+		
+		if(!ObjectUtils.isEmpty(updateCategory)){
+			
+			if(!file.isEmpty()) {
+				File saveFile= new ClassPathResource("static/img").getFile();
+			    Path path =	Paths.get(saveFile.getAbsolutePath()+File.separator+"category_img"+File.separator+file.getOriginalFilename());
+			    
+			    System.out.println(path);
+			    
+			    Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			}
+			
+			session.setAttribute("succMsg", "category update successfully");
+		}else {
+			session.setAttribute("errorMsg", "something wrong on server");
+		}
+		
+		
+		return  "redirect:/admin/loadEditCategory/" + category.getId();
 	}
 	
 }
